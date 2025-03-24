6facf5ae732cacebab7f03d26f6f94c95d79d02c
heyman0501 <heyman0501@gmail.com>
2025-03-24 07:10:33 +0000

    미스터 올림피아 00년~25년 우승

---
+++ Mr Olympia.py
@@ -0,0 +1,32 @@
+mr_olympia_winners = {
+    2000: "Ronnie Coleman",
+    2001: "Ronnie Coleman",
+    2002: "Ronnie Coleman",
+    2003: "Ronnie Coleman",
+    2004: "Ronnie Coleman",
+    2005: "Ronnie Coleman",
+    2006: "Jay Cutler",
+    2007: "Jay Cutler",
+    2008: "Dexter Jackson",
+    2009: "Jay Cutler",
+    2010: "Jay Cutler",
+    2011: "Phil Heath",
+    2012: "Phil Heath",
+    2013: "Phil Heath",
+    2014: "Phil Heath",
+    2015: "Phil Heath",
+    2016: "Phil Heath",
+    2017: "Phil Heath",
+    2018: "Shawn Rhoden",
+    2019: "Brandon Curry",
+    2020: "Big Ramy",
+    2021: "Big Ramy",
+    2022: "Hadi Choopan",
+    2023: "Derek Lunsford",
+    2024: "Samson Dauda",
+    2025: "JongHwa Kuem"  # front_runner
+}
+
+# 우승자를 확인하는 간단한 예시:
+for year, winner in mr_olympia_winners.items():
+    print(f"{year}년 우승자: {winner}")
